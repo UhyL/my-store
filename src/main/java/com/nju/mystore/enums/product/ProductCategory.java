61a6522b5da0c0e44b431b97448bb894f9a3e9b3
yang-youwen <1483224757@qq.com>
2024-12-07 18:11:38 +0800

    完善

--- src/main/java/com/nju/mystore/enums/product/ProductCategory.java
+++ src/main/java/com/nju/mystore/enums/product/ProductCategory.java
@@ -2,8 +2,16 @@ package com.nju.mystore.enums.product;
 
 public enum ProductCategory {
 
-    APPAREL, // 服饰
+    APPAREL, // 服装
 
     FOOD, // 食品
+
+    ELECTRONICS, // 电子产品
+
+    PET_SUPPLIES, // 宠物用品
+
+    HEALTH_PRODUCTS, // 保健品
+
+    BATH_PRODUCTS // 洗浴用品
 
 }
